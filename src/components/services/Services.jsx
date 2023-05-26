5073ed9ed955ecdd49f6fcdda257e921dfc1a794
kvngore <oreoluwalawanson@gmail.com>
2023-05-26 02:13:13 -0600

    Deployed website

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -11,13 +11,13 @@ const Services = () => {
       <div className="container services__container">
         <article className="service">
           <div className="service__head">
-            <h3>Web Development</h3>
+            <h3>Web Design Implementation</h3>
           </div>
 
           <ul className="service__list">
             <li> 
               <BiCheck className='service__list-icon' />
-              <p>I like building things from the ground up and bringing them to life in the browser</p>
+              <p>I am capable of taking designs created by UX/UI designers and converting them into functional web pages. By using HTML, CSS and JavaScript to translate the design mockups into code.</p>
             </li>
             
           </ul>
@@ -26,13 +26,28 @@ const Services = () => {
 
         <article className="service">
           <div className="service__head">
-            <h3>Web Maintenance</h3>
+            <h3>Front-end Frameworks and Libraries</h3>
           </div>
 
           <ul className="service__list">
             <li> 
               <BiCheck className='service__list-icon' />
-              <p>I can handle all of your website maintenance needs quickly and efficiently, so that you can focus on growing your business instead of wasting time updating your site. I have a wealth of experience with a wide variety of CMS, Shopping Carts, Frameworks, custom dynamic sites, and of course plain HTML sites</p>
+              <p>I utilize frameworks and libraries such as React to speed up development, enhance functionality and improve user experience.</p>
+            </li>
+            
+          </ul>
+        </article>
+        {/*END OF WEB DEVELOPMENT */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Debugging and Troubleshooting</h3>
+          </div>
+
+          <ul className="service__list">
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p>I'm capable of debugging and resolving issues related to your website functionality, browser compatibility, layout inconsistencies, JavaScript errors, and other front-end-related problems.</p>
             </li>
             
           </ul>
